33bb65b017af43ea5a93379a962c1205308449d6
Skpone <mulaterofranco@gmail.com>
2023-05-17 09:27:58 -0300

    Implementamos estructura basica del game + testeo (main.js, .css .html) Estructura base en el index.html, implementamos los keyframes para el character y en main.js codigo que se uso para testear si funcionaban las implementaciones. Nuevas carpeta para clases.

--- main.js
+++ main.js
@@ -1,28 +1,53 @@
 "use strict";
 
 let in_game = true;
+let player = document.querySelector('#player');
 
-    function process_user_input() {
-        //procesar el input del usuario
+function animacionPersonaje(e) {
+    let steps;
+    switch (e.code) {
+        case "Space":
+            console.log("se apreto espacio");
+            steps = 4; /*steps que necesito para saltar*/
+            player.style.background = 'url(./sprites/Character1/Biker_jump.png) left center';
+            player.style.animation = `jump 1s steps(${steps}) infinite`;
+            break;
+        case "Backspace":
+            console.log("se apreto backspace");
+            steps = 6;
+            player.style.background = 'url(./sprites/Character1/Biker_run.png) left center';
+            player.style.animation = `run 1s steps(${steps}) infinite`;
+            break;
+        default:
+            console.log("otra tecla");
+            break;
     }
+}
 
-    function refresh_status() {
-        // actualizar estado
-    }
+//procesar el input del usuario
+function process_user_input() {
+  document.addEventListener("keydown", (e) => {
+    animacionPersonaje(e);
+  });
+}
 
-    function rendering() {
-        // dibujar juego
-    }
+function refresh_status() {
+  // actualizar estado
+}
 
-    function gameLoop() {
-        console.log('Bucle en ejecucion.');
-        process_user_input();
-        refresh_status();
-        rendering();
+function rendering() {
+  // dibujar juego
+}
 
-        if (in_game) {
-            requestAnimationFrame(gameLoop);
-        }
-    }
+function gameLoop() {
+  console.log("Bucle en ejecucion.");
+  process_user_input();
+  refresh_status();
+  rendering();
 
-    gameLoop();
+  if (in_game) {
+    requestAnimationFrame(gameLoop);
+  }
+}
+
+gameLoop();
